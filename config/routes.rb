2050d448a192252fadaff6e64c0e60c89de517e3
Kevin Ashworth <1218844+kevinashworth@users.noreply.github.com>
2026-06-15 14:25:24 -0700

    16.5. Unsubscribe Links

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
 
   root "products#index"
 end

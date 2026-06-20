497fa135c17bde587fcfb22fd6e36e60a682f85d
Kevin Ashworth <1218844+kevinashworth@users.noreply.github.com>
2026-06-20 11:36:41 -0700

    2.2. Sign Up Routes & Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   validates :first_name, :last_name, presence: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+  validates :email_address, presence: true, uniqueness: true
 
   def full_name
     "#{first_name} #{last_name}"

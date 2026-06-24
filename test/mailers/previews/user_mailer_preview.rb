a081681faaac81344782120845d59dfdd20c118e
Kevin Ashworth <1218844+kevinashworth@users.noreply.github.com>
2026-06-23 18:22:37 -0700

    test: fix failing test
    Error:
    UserMailerTest#test_email_confirmation:
    NoMethodError: undefined method 'generate_token_for' for nil
        app/mailers/user_mailer.rb:6:in 'UserMailer#email_confirmation'
        test/mailers/user_mailer_test.rb:6:in 'block in <class:UserMailerTest>'
    bin/rails test test/mailers/user_mailer_test.rb:4

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,17 @@
 class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/email_confirmation
   def email_confirmation
-    UserMailer.email_confirmation
+    UserMailer.with(user: sample_user).email_confirmation
   end
+
+  private
+    def sample_user
+      User.take || User.new(
+        email_address: "preview@example.com",
+        unconfirmed_email: "preview@example.com",
+        first_name: "Preview",
+        last_name: "User",
+        password: "password"
+      )
+    end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,10 +2,13 @@ require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
   test "email_confirmation" do
-    mail = UserMailer.email_confirmation
+    user = users(:one)
+    user.unconfirmed_email = "to@example.org"
+
+    mail = UserMailer.with(user: user).email_confirmation
     assert_equal "Email confirmation", mail.subject
     assert_equal [ "to@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Confirm your email", mail.body.encoded
   end
 end
